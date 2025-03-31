dd3a19dc7700e744c51df649c42eb90c84f2506a
AdityasArsenal <24adityapatil@gmail.com>
2025-03-31 15:09:37 +0530

    it begins again
    The shadsn lib will replace my need to learn front-end
    Ha! nhaaa!

---
+++ front-end/app/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from "@/components/hero-section";
+import ContentSection from "@/components/content-7";
+import Features8 from "@/components/features-8";
+import Stats from "@/components/stats"
+import Testimonials from "@/components/testimonials"
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <Features8 />
+      <Stats />
+      <Testimonials />
+    </div>
+  );
+}
